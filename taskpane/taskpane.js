1ba2fa890ca53b4c2e128b87fdd34a413a5cde64
ZiadIMA <ziad.oueslati@ima.it>
2024-04-29 12:45:02 +0200

    js

--- taskpane/taskpane.js
+++ taskpane/taskpane.js
@@ -6,32 +6,27 @@ Office.onReady((info) => {
 
 function addUrgency() {
   const item = Office.context.mailbox.item;
-  const select = document.getElementById("urgencySelect"); // Correction de l'ID
+  const select = document.getElementById("urgencySelect");
   const urgencyValue = select.value;
   const urgencyText = select.options[select.selectedIndex].text;
-  const machineName = document.getElementById("machineName").value; // Récupérer le nom de la machine
-  const lineName = document.getElementById("lineName").value; // Récupérer le nom de la ligne
+  const machineName = document.getElementById("machineName").value;
+  const lineName = document.getElementById("lineName").value;
 
-  // Utiliser getAsync pour récupérer le sujet de l'élément
   item.subject.getAsync((asyncResult) => {
     if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-      console.error(asyncResult.error.message); // Utilisation de console.error au lieu de write
+      console.error(asyncResult.error.message);
       return;
     }
 
     let subject = asyncResult.value;
-    const prefixRegex = /\[[^\]]+\]/; // Expression régulière pour trouver le préfixe existant
+    const prefixRegex = /\[[^\]]+\]/;
 
-    // Vérifier si le sujet contient déjà un préfixe
     if (prefixRegex.test(subject)) {
-      // Remplacer le préfixe existant par le nouveau
       subject = subject.replace(prefixRegex, `[${lineName}/${machineName}/@${urgencyValue}]`);
     } else {
-      // Ajouter le préfixe au début du sujet
       subject = `[${lineName}/${machineName}/@${urgencyValue}] - ${subject}`;
     }
 
-    // Mettre à jour le sujet avec la nouvelle valeur
     item.subject.setAsync(subject, { coercionType: Office.CoercionType.Text });
   });
 }
